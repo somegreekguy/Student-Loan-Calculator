77033c6520ff952a8480087a5b4fb70ceacfba15
Siraj Rauff <sirajrauff@gmail.com>
2016-01-10 17:42:48 -0500

    Added Calculator Functionality with Initial Parameters

--- src/servlets/Start.java
+++ src/servlets/Start.java
@@ -2,9 +2,12 @@ package servlets;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Enumeration;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -34,6 +37,41 @@ public class Start extends HttpServlet {
 		response.setContentType("text/plain");
 		Writer resOut = response.getWriter();
 
+		ServletContext context = getServletContext();
+		double principal = Double.parseDouble(context.getInitParameter("principal"));
+		double interest = Double.parseDouble(context.getInitParameter("interest"));
+		int period = Integer.parseInt(context.getInitParameter("period"));
+
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie: cookies) {
+				if (cookie.getName().equals("principal")) {
+					principal = Double.parseDouble(cookie.getValue());
+				} else if (cookie.getName().equals("period")) {
+					period = Integer.parseInt(cookie.getValue());
+				}
+			}
+		}
+
+		Enumeration<String> params = request.getParameterNames();
+		while (params.hasMoreElements()) {
+			String param = params.nextElement();
+			if (param.equals("principal")) {
+				String value = request.getParameter("principal");
+				principal = Double.parseDouble(value);
+				response.addCookie(new Cookie("principal", value));
+			} else if (param.equals("interest")) {
+				interest = Double.parseDouble(request.getParameter("interest"));
+			} else if (param.equals("period")) {
+				String value = request.getParameter("period");
+				period = Integer.parseInt(value);
+				response.addCookie(new Cookie("period", value));
+			}
+		}
+
+		double mnthIntrst = interest / 12;
+		double payment = ( mnthIntrst * principal )/( 1 - Math.pow( 1 + mnthIntrst, -period ));
+
 		String IPAddress = request.getRemoteAddr();
 		String protocol = request.getProtocol();
 		String method = request.getMethod();
@@ -50,6 +88,9 @@ public class Start extends HttpServlet {
 				+ "\nServlet: " + request.getServletPath()
 				+ "\nPath: " + request.getPathInfo()
 				+ "\nContext Parameter Test: " + getServletContext().getInitParameter("appName")
+				+ "\n--------------------------------------------------"
+				+ String.format( "\nPrincipal amount of $%.2f. Interest rate of %.2f. Period of %d months."
+					+ "\nYour monthly payment will be: $%.2f", principal, interest, period, payment )
 				);
 	}
 
