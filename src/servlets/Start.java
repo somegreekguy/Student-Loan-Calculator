704add6dc7df27cef7073d531cad5fc14a9dc627
Siraj Rauff <sirajrauff@gmail.com>
2016-01-17 19:03:22 -0500

    Removed re-computation support and Networking Explorations

--- src/servlets/Start.java
+++ src/servlets/Start.java
@@ -7,7 +7,6 @@ import java.util.Enumeration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -37,21 +36,12 @@ public class Start extends HttpServlet {
 		response.setContentType("text/plain");
 		Writer resOut = response.getWriter();
 
+		// Get Servlet Context that contains parameters
 		ServletContext context = getServletContext();
+
 		double principal = Double.parseDouble(context.getInitParameter("principal"));
 		double interest = Double.parseDouble(context.getInitParameter("interest"));
 		int period = Integer.parseInt(context.getInitParameter("period"));
-
-		Cookie[] cookies = request.getCookies();
-		if (cookies != null) {
-			for (Cookie cookie: cookies) {
-				if (cookie.getName().equals("principal")) {
-					principal = Double.parseDouble(cookie.getValue());
-				} else if (cookie.getName().equals("period")) {
-					period = Integer.parseInt(cookie.getValue());
-				}
-			}
-		}
 
 		Enumeration<String> params = request.getParameterNames();
 		while (params.hasMoreElements()) {
@@ -59,13 +49,11 @@ public class Start extends HttpServlet {
 			if (param.equals("principal")) {
 				String value = request.getParameter("principal");
 				principal = Double.parseDouble(value);
-				response.addCookie(new Cookie("principal", value));
 			} else if (param.equals("interest")) {
 				interest = Double.parseDouble(request.getParameter("interest"));
 			} else if (param.equals("period")) {
 				String value = request.getParameter("period");
 				period = Integer.parseInt(value);
-				response.addCookie(new Cookie("period", value));
 			}
 		}
 
@@ -77,19 +65,7 @@ public class Start extends HttpServlet {
 		String method = request.getMethod();
 		String query = request.getQueryString();
 		int port = request.getRemotePort();
-		resOut.write("IP Address: " + IPAddress
-				+ "\nPort: " + port
-				+ "\nProtocol: " + protocol
-				+ "\nMethod: " + method
-				+ "\nQuery String: " + query
-				+ "\nParameter: " + request.getParameter("Test")
-				+ "\nURI: " + request.getRequestURI()
-				+ "\nContext: " + request.getContextPath()
-				+ "\nServlet: " + request.getServletPath()
-				+ "\nPath: " + request.getPathInfo()
-				+ "\nContext Parameter Test: " + getServletContext().getInitParameter("appName")
-				+ "\n--------------------------------------------------"
-				+ String.format( "\nPrincipal amount of $%.2f. Interest rate of %.2f. Period of %d months."
+		resOut.write( String.format( "\nPrincipal amount of $%.2f. Interest rate of %.2f. Period of %d months."
 					+ "\nYour monthly payment will be: $%.2f", principal, interest, period, payment )
 				);
 	}
