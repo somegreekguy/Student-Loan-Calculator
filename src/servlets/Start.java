27f6514f041abf385ccb5ee51c2aadc2360a1909
Siraj Rauff <sirajrauff@gmail.com>
2016-01-17 20:31:40 -0500

    Changed Form Submission to POST

--- src/servlets/Start.java
+++ src/servlets/Start.java
@@ -1,7 +1,6 @@
 package servlets;
 
 import java.io.IOException;
-import java.io.Writer;
 import java.text.DecimalFormat;
 import java.util.Enumeration;
 
@@ -30,55 +29,50 @@ public class Start extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if (request.getParameter("submit") == null) {
-				request.getRequestDispatcher("/UI.jspx").forward(request, response);
-				return;
-		}
-
-		// Get Servlet Context that contains parameters
-		ServletContext context = getServletContext();
-
-		double principal = Double.parseDouble(context.getInitParameter("principal"));
-		double interest = Double.parseDouble(context.getInitParameter("interest"));
-		double fixedInterest = Double.parseDouble(context.getInitParameter("fixedInterest"));
-		int period = Integer.parseInt(context.getInitParameter("period"));
-		int gracePeriod = Integer.parseInt(context.getInitParameter("gracePeriod"));
-		boolean grace = false;
-
-		Enumeration<String> params = request.getParameterNames();
-		while (params.hasMoreElements()) {
-			String param = params.nextElement();
-			String value = request.getParameter(param);
-
-			if (value.equals("")){
-			} else if (param.equals("principal")) {
-				principal = Double.parseDouble(value);
-			} else if (param.equals("interest")) {
-				interest = Double.parseDouble(value);
-			} else if (param.equals("period")) {
-				period = Integer.parseInt(value);
-			} else if (param.equals("grace")) {
-				grace = value.equals("on") ? true : false;
-			}
-		}
-
-		// Calculations
-		double graceInterest = grace ? principal * (( interest/100 + fixedInterest/100 ) / 12 ) * gracePeriod : 0;
-		double mnthIntrst = ( interest / 100 ) / 12;
-		double actualPeriod = grace ? period - gracePeriod : period;
-		double payment = ( mnthIntrst * principal )/( 1 - Math.pow( 1 + mnthIntrst, -actualPeriod ));
-		payment = grace ? payment + ( graceInterest / gracePeriod ) : payment;
-
-		context.setAttribute("graceInterest", new DecimalFormat("##.00").format(graceInterest));
-		context.setAttribute("payment", new DecimalFormat("##.00").format(payment));
-		request.getRequestDispatcher("/Results.jspx").forward(request, response);
+		request.getRequestDispatcher("/UI.jspx").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	// Get Servlet Context that contains parameters
+	ServletContext context = getServletContext();
+
+	double principal = Double.parseDouble(context.getInitParameter("principal"));
+	double interest = Double.parseDouble(context.getInitParameter("interest"));
+	double fixedInterest = Double.parseDouble(context.getInitParameter("fixedInterest"));
+	int period = Integer.parseInt(context.getInitParameter("period"));
+	int gracePeriod = Integer.parseInt(context.getInitParameter("gracePeriod"));
+	boolean grace = false;
+
+	Enumeration<String> params = request.getParameterNames();
+	while (params.hasMoreElements()) {
+		String param = params.nextElement();
+		String value = request.getParameter(param);
+
+		if (value.equals("")){
+		} else if (param.equals("principal")) {
+			principal = Double.parseDouble(value);
+		} else if (param.equals("interest")) {
+			interest = Double.parseDouble(value);
+		} else if (param.equals("period")) {
+			period = Integer.parseInt(value);
+		} else if (param.equals("grace")) {
+			grace = value.equals("on") ? true : false;
+		}
+	}
+
+	// Calculations
+	double graceInterest = grace ? principal * (( interest/100 + fixedInterest/100 ) / 12 ) * gracePeriod : 0;
+	double mnthIntrst = ( interest / 100 ) / 12;
+	double actualPeriod = grace ? period - gracePeriod : period;
+	double payment = ( mnthIntrst * principal )/( 1 - Math.pow( 1 + mnthIntrst, -actualPeriod ));
+	payment = grace ? payment + ( graceInterest / gracePeriod ) : payment;
+
+	context.setAttribute("graceInterest", new DecimalFormat("##.00").format(graceInterest));
+	context.setAttribute("payment", new DecimalFormat("##.00").format(payment));
+	request.getRequestDispatcher("/Results.jspx").forward(request, response);
 	}
 
 }
